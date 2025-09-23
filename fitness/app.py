3dafc2a73c56f628b9aad5189d85b285dfbbe2f1
Xavier Bednarek @CN-Gre <xavier.bednarek@le-campus-numerique.fr>
2025-09-23 15:31:55 +0200

    Ecran titre

--- fitness/app.py
+++ fitness/app.py
@@ -32,6 +32,7 @@ class App :
         self.cap = cv2.VideoCapture(0)
         # Réglage de la verbosité
         self.verbose = verbose
+
         # Construction des detecteurs :
         # Pompes
         self.push_up_detector = PushUpDetector(self.mediapipe_model, self.cap, self.verbose)
@@ -41,6 +42,12 @@ class App :
         self.squats_detector = SquatDetector(self.mediapipe_model, self.cap, self.verbose)
         # Montée de genou
         self.knee_raise_detector = KneeRaiseDetector(self.mediapipe_model, self.cap, self.verbose)
+
+        # Positions de cliques de la souris
+        self.left_clicked_x = -1
+        self.left_clicked_y = -1
+        self.right_clicked_x = -1
+        self.right_clicked_y = -1
 
 
     def __del__(self):
@@ -66,6 +73,58 @@ class App :
     #                                                                  Méthodes
     # -------------------------------------------------------------------------
     
+    def show_start_screen(self) -> bool :
+        """Affichage de l'écran de démarrage de l'application"""
+
+        # Variable de sortie :
+        go  = False
+
+        # Chargement de l'écran de démarrage
+        screen = cv2.imread('fitness/logo/screen_1.png')
+
+        # Zone de textes cliquables
+        button_go = {'rect_start': (295, 400),
+                     'rect_end': (345, 450),
+                     "line_color": (0, 255, 0),
+                     "line_thickness": 2,
+                     'text': 'GO !',
+                     "text_color": (0, 255, 0),
+                     "text_font": cv2.FONT_HERSHEY_SIMPLEX,
+                     "text_thickness": 1,
+                     "text_font_scale": 0.5}
+
+        # Ajouts des zones cliquables
+        cv2.rectangle(screen, button_go['rect_start'], button_go['rect_end'], button_go['line_color'], button_go['line_thickness'])
+        text_position = ( int(0.8 * button_go['rect_start'][0] + 0.2 * button_go['rect_end'][0]),
+                          int(0.8 * button_go['rect_start'][1] + 0.2 * button_go['rect_end'][1]))
+        cv2.putText(screen, button_go['text'], text_position, button_go['text_font'], button_go['text_font_scale'], button_go['text_color'], button_go['text_thickness'], cv2.LINE_AA)
+
+        # Gestion du callback de la souris
+        win = "First screen"
+        cv2.namedWindow(win)
+        cv2.setMouseCallback(win, self.mouse_callback)
+
+        while  self.cap.isOpened(): 
+
+            # Affichage
+            cv2.imshow(win, screen)
+
+            # Capture du clic
+            if (self.left_clicked_x >= button_go['rect_start'][0] and 
+                self.left_clicked_y >= button_go['rect_start'][1] and
+                self.left_clicked_x <= button_go['rect_end'][0] and 
+                self.left_clicked_y <= button_go['rect_end'][1] ) :
+                if self.verbose :
+                    print(button_go['text'])
+                go = True
+                break
+            
+            # Quitter au clavier
+            if cv2.waitKey(5) & 0xFF == ord('q'):
+                break
+
+        return go
+
     # Lancement d'un set d'exercice
     def run_exercice_session(self, exercices:dict) :
         """ Lancement d'un session d'exercice
@@ -87,6 +146,26 @@ class App :
         for exo, score in scores.items():
             print(f"Score : {score:.1f} {exo}.")
 
+    # -------------------------------------------------------------------------
+    #                                                 Méthodes pour l'affichage
+    # -------------------------------------------------------------------------
+
+    def mouse_callback(self, event, x, y, flags, param):
+        """Définition du callback pour le clique souris"""
+
+        if event == cv2.EVENT_LBUTTONDOWN:
+            if self.verbose :
+                print(f"Clic gauche à ({x}, {y})")
+            self.left_clicked_x = x
+            self.left_clicked_y = y
+        elif event == cv2.EVENT_RBUTTONDOWN:
+            if self.verbose :
+                print(f"Clic droit à ({x}, {y})")
+            self.right_clicked_x = x
+            self.right_clicked_y = y
+        
+
+
 
 if __name__=='__main__':
     # Tests
@@ -105,9 +184,13 @@ if __name__=='__main__':
             cst.EX_SQUATS: 5,
             cst.EX_PUSH_UP: 7}
     
+    # Run de l'écran test :
+    go = app.show_start_screen()
+    
     # Run du set :
     print("-"*10)
     print(" * Run d'exercices *")
-    app.run_exercice_session(exos)
+    if go :
+        app.run_exercice_session(exos)
 
-    print("--")
+    
