992ee9a8396c896b0d1eea68b4777403ec745038
Pauline Faure <pauline.faure@le-campus-numerique.fr>
2025-09-24 12:25:30 +0200

    detect plank exercice

--- fitness/constants.py
+++ fitness/constants.py
@@ -9,12 +9,16 @@ RIGHT_SHOULDER = mp.solutions.pose.PoseLandmark.RIGHT_SHOULDER
 RIGHT_ELBOW = mp.solutions.pose.PoseLandmark.RIGHT_ELBOW
 RIGHT_WRIST = mp.solutions.pose.PoseLandmark.RIGHT_WRIST
 RIGHT_HIP = mp.solutions.pose.PoseLandmark.RIGHT_HIP
+RIGHT_ANKLE = mp.solutions.pose.PoseLandmark.RIGHT_ANKLE
+RIGHT_KNEE = mp.solutions.pose.PoseLandmark.RIGHT_KNEE
 
 # Coté gauche
 LEFT_SHOULDER = mp.solutions.pose.PoseLandmark.LEFT_SHOULDER
 LEFT_ELBOW = mp.solutions.pose.PoseLandmark.LEFT_ELBOW
 LEFT_WRIST = mp.solutions.pose.PoseLandmark.LEFT_WRIST
 LEFT_HIP = mp.solutions.pose.PoseLandmark.LEFT_HIP
+LEFT_ANKLE = mp.solutions.pose.PoseLandmark.LEFT_ANKLE
+LEFT_KNEE = mp.solutions.pose.PoseLandmark.LEFT_KNEE
 
 
 # Nom des exercices
@@ -22,6 +26,7 @@ EX_PUSH_UP = "Pompes"
 EX_HEELS2BUTTOCKS = "Talons-fesses"
 EX_KNEERAISE = "Montées de genoux"
 EX_SQUATS = "Squats"
+EX_PLANK = "Planche"
 
 # MOUVEMENTS 
 MOVE_UP = "move up"
@@ -41,3 +46,4 @@ WIN_NAME_PUSHUP = "Pompes !"
 WIN_NAME_HEELS2BUTTOCKS= "Talons fesses !"
 WIN_NAME_KNEERAISE = "Montees de genoux !"
 WIN_NAME_SQUATS = "Squats !"
+WIN_NAME_PLANK = "Planche!"
---
+++ fitness/plankdetector.py
@@ -0,0 +1,179 @@
+# Import relatif dans le package :
+from .detector import Detector
+from . import constants as cst # <-- Pour utiliser les constantes
+from .tools import image2position, calcul_angle
+# (Pour exécuter ce fichier, il faut donc faire proprement depuis l'extérieur du package)
+# Exemple : uv run python3 -m fitness.pushupdetector
+import time
+
+# Autres imports
+import numpy as np
+import cv2
+import joblib
+
+class PlankDetector(Detector):
+
+    # -------------------------------------------------------------------------
+    #                                                              Constructeur
+    # -------------------------------------------------------------------------
+
+    def __init__(self, mediapipe_model, cap, verbose:bool = False) -> None:
+        """Constructeur"""
+        # Appel explicite du constructeur parent
+        super().__init__(mediapipe_model, cap, verbose)
+
+
+    # -------------------------------------------------------------------------
+    #                                                                  Méthodes
+    # -------------------------------------------------------------------------
+    
+    # Masquage
+
+
+    def run(self, objective:int) -> int:
+            
+            plank_active = False
+            start_time = 0
+            elapsed = 0
+            tolerance_time = 5
+            tolerance_start = None
+
+            while self.cap.isOpened():
+                 
+                success, image = self.cap.read()
+
+                if not success:
+                    print("Ignoring empty camera frame")
+                    continue
+
+                #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                position = image2position(image, mediapipe_model=self.mediapipe_model, show=False)
+
+                if position is None:
+                    detection = False
+                else:
+                    detection = self.detect(position)  
+
+                # --------- Detect start plank and end plank ---------
+                if detection:
+                    if not plank_active:
+                        # plank starting
+                        plank_active = True
+                        # start time
+                        start_time = time.time()
+                        if self.verbose:
+                            print("Plank started!")
+                    # if plank strat, tolerance is reset
+                    tolerance_start = None
+                else:
+                    # not detection but the person was already in plank
+                    if plank_active:
+                        if tolerance_start is None:
+                            # strat tolerance
+                            tolerance_start = time.time()
+                        # if tme is up to tolerence, the exercie end
+                        elif time.time() - tolerance_start >= tolerance_time:
+                            plank_active = False
+                            # check the total time
+                            elapsed = time.time() - start_time
+                            if self.verbose:
+                                print(f"Plank stopped! Duration: {elapsed:.2f}s")
+                            # end the exercice
+                            break  
+
+                # --------- retrieve timer ---------
+                if plank_active:
+                    elapsed = time.time() - start_time
+
+                # --------- Display timmer ---------
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                font_scale = 0.7
+                color = (0, 0, 255)
+                thickness = 2
+                text = f"Plank: {elapsed:.1f} s"
+                cv2.putText(image, text, (10,50), font, font_scale, color, thickness, cv2.LINE_AA)
+                cv2.imshow(cst.WIN_NAME_PLANK, image)
+
+                # --------- Set up "q" to quite ---------
+                if cv2.waitKey(5) & 0xFF == ord('q'):
+                    break
+
+            cv2.destroyWindow(cst.WIN_NAME_PLANK)
+            return elapsed
+
+    
+    def detect(self, positions):
+        """detect if the person is doing plank exercice"""
+        return self.is_elbow_ankle_aligned_y(positions) and self.check_plank_angle(positions)
+            
+    def is_elbow_ankle_aligned_y(self,position): 
+        """Check if the elbows and ankles are approximately aligned along the vertical axis (y),
+           to ensure the person is in a proper plank position.
+
+        Args:
+            position (list of Landmark): 
+            A list of 33 MediaPipe Pose landmarks. Each landmark should have a `.y`.
+
+        Returns:
+            bool: 
+                True if the elbows and ankles are approximately aligned vertically (within tolerance), 
+                False otherwise.
+    
+        """
+
+        # retriev the y coordinate
+        right_elbow = position[cst.RIGHT_ELBOW][1]
+        left_elbow  = position[cst.LEFT_ELBOW][1]
+        right_ankle = position[cst.RIGHT_ANKLE][1]
+        left_ankle  = position[cst.LEFT_ANKLE][1]
+
+        tolerance = 0.1
+        y_values = [right_ankle,left_ankle, right_elbow, left_elbow]
+
+        # chek if ankel and elbow are more or less at the same y
+        return max(y_values)-min(y_values) < tolerance
+    
+    def check_plank_angle(self,position) :
+
+        """Check if the shoulder-hip-knee angles are approximately straight on at least one side
+            to verify a proper plank posture.
+
+        Args:
+            position (list of Landmark): 
+                A list of 33 MediaPipe Pose landmarks. Each landmark should have `.x`, `.y`, 
+                `.z` attributes.
+
+        Returns:
+            bool: 
+                True if the plank is approximately straight on at least one side, False otherwise.
+
+        """
+
+        # retrieve coordinate needed
+        right_knee_coordinate = position[cst.RIGHT_KNEE]
+        left_knee_coordinate  = position[cst.LEFT_KNEE]
+        right_hip_coordinate = position[cst.RIGHT_HIP]
+        left_hip_coordinate  = position[cst.LEFT_HIP]
+        right_shoulder_coordinate = position[cst.RIGHT_SHOULDER]
+        left_shoulder_coordinate  = position[cst.LEFT_SHOULDER]
+        #right_elbow_coordinate = position[cst.RIGHT_ELBOW]
+        #left_elbow_coordinate  = position[cst.LEFT_ELBOW]
+
+        # calcul angle  shoulder hip knee
+        angle_right = calcul_angle(right_shoulder_coordinate,right_hip_coordinate,right_knee_coordinate)
+        angle_left = calcul_angle (left_shoulder_coordinate,left_hip_coordinate,left_knee_coordinate)
+
+        # tolerance around 180°
+        return 160< angle_right < 190 or 160< angle_left < 190
+
+
+
+
+    
+
+
+
+
+
+
+        
